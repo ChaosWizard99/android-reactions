32889829b0557955047a3a8ddfba9f865e4fdd8c
ChaosWizard99 <35845968+ChaosWizard99@users.noreply.github.com>
2019-08-05 00:02:07 -0500

    Update ReactionViewGroup.kt

--- library/src/main/java/com/github/pgreze/reactions/ReactionViewGroup.kt
+++ library/src/main/java/com/github/pgreze/reactions/ReactionViewGroup.kt
@@ -55,7 +55,7 @@ class ReactionViewGroup(context: Context, private val config: ReactionsConfig) :
 
     private val background = RoundedView(context, config)
             .also {
-                it.layoutParams = LayoutParams(dialogWidth, dialogHeight).gravity = Gravity.RIGHT|Gravity.CENTER_VERTICAL;
+                it.layoutParams = LayoutParams(dialogWidth, dialogHeight).gravity = Gravity.RIGHT|Gravity.CENTER_VERTICAL
                 addView(it)
             }
     private val reactions: List<ReactionView> = config.reactions
